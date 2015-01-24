13a3d610881d09d3e312eb29f05a0bf4ab364de8
Malene Sjoerslev Soeholm <malene.soeholm@gmail.com>
2015-01-24 16:27:07 +0100

    Added linesof

---
+++ util/linesof.m
@@ -0,0 +1,32 @@
+function [lines] = linesof ( I )
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+	I = rgb2gray(I);
+	BW = edge(I,'canny',0.4);
+	
+	[H, t, r] = hough(BW);
+	P = houghpeaks(H, 20, 'threshold', ceil(0.3*max(H(:))));
+
+	x = t(P(:,2));
+	y = r(P(:,1));
+
+	lines = houghlines(BW, t, r, P, 'FillGap', 60, 'Minlength', 200);
+
+	figure, imshow(BW), hold on
+	max_len = 0;
+	for k = 1:length(lines)
+		xy = [lines(k).point1; lines(k).point2];
+		plot(xy(:,1),xy(:,2),'LineWidth',2,'Color','green');
+
+		% Plot beginnings and ends of lines
+		plot(xy(1,1),xy(1,2),'x','LineWidth',2,'Color','yellow');
+		plot(xy(2,1),xy(2,2),'x','LineWidth',2,'Color','red');
+
+		% Determine the endpoints of the longest line segment
+		len = norm(lines(k).point1 - lines(k).point2);
+		if ( len > max_len)
+			max_len = len;
+			xy_long = xy;
+		end
+	end
