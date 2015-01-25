927458ca6d797d5c32f4fca69fb86d1451022806
Christian Budde Christensen <budde377@gmail.com>
2015-01-25 16:28:31 +0100

    Moving around like a boss

--- example_meanof.m
+++
@@ -1,8 +0,0 @@
-function [m] = example_meanof ( )
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-
-	addpath('util');
-
-	I = imread('images/2.png', 'png');
-	m = meanof(I);
